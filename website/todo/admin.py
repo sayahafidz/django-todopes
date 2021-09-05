8c70a9987e29bd3792cbc3b89fdaa7ec8921e121
Hafidz Alfatih <hafidzalfatih04@gmail.com>
2021-09-05 13:32:11 +0700

    finish

--- website/todo/admin.py
+++ website/todo/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import Todo
 # Register your models here.
+
+# kustomisasi django admin
+
+
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'status',)
+
+
+admin.site.register(Todo, TodoAdmin)
--- website/todo/models.py
+++ website/todo/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Todo(models.Model):
+    title = models.TextField(max_length=200)
+    status = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        return self.title
